29c974b8a37870eeeefc926468632aecffdad12f
Ajinkya2298 <ajinkya.badave@toasttab.com>
2022-11-08 18:53:30 +0530

    Fixed review comments

--- dropwizardapp/src/main/kotlin/com/kotlinmonorepo/dropwizardapp/api/VendorsModel.kt
+++ dropwizardapp/src/main/kotlin/com/kotlinmonorepo/dropwizardapp/api/VendorsModel.kt
@@ -8,23 +8,5 @@ data class VendorsModel (
     @JsonProperty("name")
     val name: String,
     @JsonProperty("fulfillmentAreas")
-    var fulfillmentAreas: Array<String>
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as VendorsModel
-
-        if (name != other.name) return false
-        if (!fulfillmentAreas.contentEquals(other.fulfillmentAreas)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + fulfillmentAreas.contentHashCode()
-        return result
-    }
-}
+    var fulfillmentAreas: List<String>
+)
--- dropwizardapp/src/main/kotlin/com/kotlinmonorepo/dropwizardapp/entities/VendorsEntity.kt
+++ dropwizardapp/src/main/kotlin/com/kotlinmonorepo/dropwizardapp/entities/VendorsEntity.kt
@@ -6,26 +6,5 @@ import java.util.*
 data class VendorsEntity(
     val id: UUID,
     var name: String,
-    var fulfillmentAreas:Array<String>
-
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as VendorsEntity
-
-        if (id != other.id) return false
-        if (name != other.name) return false
-        if (!fulfillmentAreas.contentEquals(other.fulfillmentAreas)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + fulfillmentAreas.contentHashCode()
-        return result
-    }
-}
+    var fulfillmentAreas:List<String>
+)
